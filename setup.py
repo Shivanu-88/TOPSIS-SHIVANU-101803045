0ca6db3c69c6f6dece05907c9f435cb29a85eed8
Shivanu Sharma <shivanupaliwal@gmail.com>
2020-11-13 22:07:17 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Shivanu Sharma',                   # Type in your name
   author_email = 'shivanupaliwal@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/Shivanu-88',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/Shivanu-88/TOPSIS-SHIVANU-101803045.git',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'validators',
